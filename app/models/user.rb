091b6068071141590e7e693333454ec987962b0b
mikuto17 <ishi17hara@gmail.com>
2023-12-01 18:01:42 +0900

    レビューの修正３

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname, presence: true
-  validates :password, presence: true,
+  validates :password
             format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/, message: "は半角英数字の組み合わせで入力してください" }
   validates :first_name, presence: true,
             format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/,message: "は全角文字で入力してください" }
